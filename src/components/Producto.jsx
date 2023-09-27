ef3007ed8336bbaf0b19352ea2bf4a17a6e50f57
Eduardo <eduardogomez.sent2@gmail.com>
2023-09-27 07:49:49 -0600

    carrito v2

--- src/components/Producto.jsx
+++ src/components/Producto.jsx
@@ -3,33 +3,49 @@ import '../assets/style.css'
 
 export default function Producto({precio, productName, setTotal, total ,imagen}) {
 
-  //const colors = ["red", "blue", "green","gray","yellow","violet", "orange"];
-  //const color = colors[Math.floor(Math.random() * colors.length)];
+  const colors = ["red", "blue", "green","gray","violet", "orange"];
+  const color = colors[Math.floor(Math.random() * colors.length)];
 
   const [count, setCount] = useState(0);
+  const [colour, setColour] = useState(color);
+  const [colorFavorito, setColorFavorito] = useState('white');
 
   const sumar = () => {
     if(count>=10){
   }else{
     setCount(count + 1)
-      setTotal(total + precio)}
+      setTotal(total + precio)};
+      if(count >=0){
+        setColour('black');
+      }
   }
   const restar = () => {
     if(count<=0){
     }else{
       setCount(count - 1)
-      setTotal(total - precio)}
+      setTotal(total - precio)};
+      if(count ==1){
+        setColour(color)};
+  }
+
+  const agregarFavorito = () => {
+    if(colorFavorito == 'white'){
+      setColorFavorito('yellow');
+    }else{
+      setColorFavorito('white');
+    }
   }
 return (
-  <div className='center producto' style={{backgroundColor: count>0 ? "red": "gray"}}>
-  <img className='producto-imagen' src={imagen} alt="" />
-  <h2>{productName}</h2>
-  <h3>precio:${precio}</h3>
-  <div>
-  <button onClick={sumar} >+</button>
-  <button>{count}</button>
-  <button onClick={restar}>-</button>
-  </div>
+  <div className='center producto' style={{backgroundColor: colour}}>
+    <p onClick={agregarFavorito} className='favorito' style={{color: colorFavorito}}>★</p>
+    <img className='producto-imagen' src={imagen} alt="" />
+    <h2>{productName}</h2>
+    <h3>precio:${precio}</h3>
+    <div>
+      <button onClick={sumar} >+</button>
+      <button>{count}</button>
+      <button onClick={restar}>-</button>
+    </div>
   </div>
   
 )
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import TiendaVirtual from './components/TiendaVirtual'
+//import JuegoReto from './components/JuegoReto'
+//import UseStateAyO from './UseStateAyO';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <TiendaVirtual />
+    < TiendaVirtual/>
   </React.StrictMode>,
 )
